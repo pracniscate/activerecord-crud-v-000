c19e7ee8166f9f1a1fe01c86f31cd11c997c0eae
Lorena <lorena.giddens@outlook.com>
2018-07-25 02:15:04 +0000

    automatically backed up by learn

--- db/migrate/20180725021053_create_movies.rb
+++ db/migrate/20180725021053_create_movies.rb
@@ -2,6 +2,10 @@ class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies |t|
       t.string :title
+      t.datetime :release_date
+      t.string :director
+      t.string :lead
+      t.datetime :in_theaters
     end
   end
 end
