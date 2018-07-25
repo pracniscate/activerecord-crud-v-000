6d2560e693cbce66de2a38e9e72e8eb80bf27f1b
Lorena <lorena.giddens@outlook.com>
2018-07-25 02:30:40 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -42,28 +42,25 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find_by(id).title
+  Movie.find(id).title
 end
 
 def can_find_by_multiple_attributes
-  # title == "Title"
-  # release_date == 2000
-  # director == "Me"
-  __
+  Movie.find_by('title = "Title" AND release_date = 2000 AND director = "Me"')
 end
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  __
+  Movie.where('release_date > 2002').order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick"
   Movie.create(title: "Awesome Flick")
-  __
-  __
-  __
+  movie = Movie.find_by('title = "Awesome Flick"')
+  movie.title = "Even Awesomer Flick"
+  movie.save
 end
 
 def can_update_using_update_method
